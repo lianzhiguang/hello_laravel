7f3c4cba2570fa00a8197c35a4be2015790c7706
lian <lianzhiguang@126.com>
2018-02-26 08:05:03 +0000

    auth test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo', function () {
+    return 'Hello World!';
+});
+
+//Route::get('/lianzhiguang', 'lianzhiguang@126.com');
+//Route::get('/user', 'UsersController@index');
+
+Route::get('/user', function () {
+    return 'lianzhiguang@126.com';
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'match: get or post.';
+});
+
+Route::any('any', function () {
+    return 'any';
+});
+
+Route::redirect('/here', '/there', 301);
